d2ae07a4e282452548479ecfddb2648e41c7a14c
dmytromayevsky <mayevskydv@gmail.com>
2024-05-22 03:26:13 -0700

    complete task 5

--- task_5.py
+++ task_5.py
@@ -6,21 +6,27 @@
 у глибину та в ширину.
 """
 
+
+
+
 import uuid
+
 import networkx as nx
 import matplotlib.pyplot as plt
+
 
 class Node:
     def __init__(self, key, color="skyblue"):
         self.left = None
         self.right = None
         self.val = key
-        self.color = color
-        self.id = str(uuid.uuid4())
+        self.color = color  # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
+
 
 def add_edges(graph, node, pos, x=0, y=0, layer=1):
     if node is not None:
-        graph.add_node(node.id, color=node.color, label=node.val)
+        graph.add_node(node.id, color=node.color, label=node.val)  # Використання id та збереження значення вузла
         if node.left:
             graph.add_edge(node.id, node.left.id)
             l = x - 1 / 2 ** layer
@@ -33,14 +39,43 @@ def add_edges(graph, node, pos, x=0, y=0, layer=1):
             r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
     return graph
 
-def generate_gradient_colors(n):
-    colors = []
-    for i in range(n):
-        r = int(18 + (180 - 18) * (i / n))
-        g = int(150 + (240 - 150) * (i / n))
-        b = int(240 + (0 - 240) * (i / n))
-        colors.append(f'#{r:02x}{g:02x}{b:02x}')
-    return colors
+
+def draw_tree(tree_root, title):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.text(pos[tree_root.id][0] - 0.8, pos[tree_root.id][1], "{}".format(title))
+    plt.show()
+
+
+def lighten_color(color, amount=0.5):
+
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
+
+
+def bfs(node):
+    visited = []
+    queue = [node]
+    while queue:
+        current = queue.pop(0)
+        if current:
+            visited.append(current)
+            queue.append(current.left)
+            queue.append(current.right)
+    return visited
 
 def dfs(node, visited):
     if node is not None:
@@ -49,41 +84,26 @@ def dfs(node, visited):
         dfs(node.right, visited)
     return visited
 
-def visualize_dfs(tree_root, title):
-    tree = nx.DiGraph()
-    pos = {tree_root.id: (0, 0)}
-    tree = add_edges(tree, tree_root, pos)
 
-    visited = dfs(tree_root, [])
-    gradient_colors = generate_gradient_colors(len(visited))
+def draw_search(root, func, title):
+    test = func
+    color = 1
+    for node in test:
+        node.color = lighten_color(node.color, color)
+        color -= 1/len(test)
+    draw_tree(root,title)
 
-    for i, node in enumerate(visited):
-        node.color = gradient_colors[i]
 
-    colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+# Створення дерева
+root = Node(0,color="blue")
+root.left = Node(4,color="blue")
+root.left.left = Node(5,color="blue")
+root.left.right = Node(10,color="blue")
+root.right = Node(1,color="blue")
+root.right.left = Node(3,color="blue")
+root.right.right = Node(10,color="blue")
 
-    plt.figure(figsize=(8, 5))
-    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
-    plt.title(title)
-    plt.show()
 
-# Example usage
-root = Node("Root", color="lightcoral")
-node1 = Node("Left", color="skyblue")
-node2 = Node("Right", color="skyblue")
-node3 = Node("Left.Left", color="lightgreen")
-node4 = Node("Left.Right", color="lightgreen")
-node5 = Node("Right.Left", color="lightyellow")
-node6 = Node("Right.Right", color="lightyellow")
-
-root.left = node1
-root.right = node2
-node1.left = node3
-node1.right = node4
-node2.left = node5
-node2.right = node6
-
-# Visualize DFS
-visualize_dfs(root, 'DFS Search Path')
+draw_search(root, bfs(root),"BFS Search path")
+draw_search(root, dfs(root,[]),"DFS Search path")
 
